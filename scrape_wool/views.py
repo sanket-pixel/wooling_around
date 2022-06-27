6444cfe222306ab041c74f41e1b92708cec0158a
sanket-pixel <sanketshah812@gmail.com>
2022-06-27 16:25:08 +0200

    Added comments to explain code

--- scrape_wool/views.py
+++ scrape_wool/views.py
@@ -8,16 +8,22 @@ from django.template import loader
 import json
 # Create your views here.
 def index(request):
+    # delete all old data from db
     WoolItem.objects.all().delete()
+    # get products and brand for which wool info is needed
     required_products_path = "data/required_products.json"
     required_products = pd.read_json(required_products_path).to_dict('records')
+    # fetch data for all items and store in db
     for wool_item in required_products:
         addItem(wool_item)
+    # get all stored items
     wool_items = getItem()
+    # send the data to the template
     template = loader.get_template('scrape_wool/index.html')
     context = {
         'available_wool_list':wool_items
     }
+    # store the data in local json file
     save_product_path = "data/saved_products.json"
     wool_json_list = []
     for wool in wool_items:
