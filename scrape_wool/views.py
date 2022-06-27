902eb43e6c3f689bcec978b7140c04010d3918f4
sanket-pixel <sanketshah812@gmail.com>
2022-06-27 15:27:40 +0200

    Added tests for wool price and link

--- scrape_wool/views.py
+++ scrape_wool/views.py
@@ -5,6 +5,7 @@ from .models import WoolItem
 import pandas as pd
 from django.forms.models import model_to_dict
 from django.template import loader
+import json
 # Create your views here.
 def index(request):
     WoolItem.objects.all().delete()
@@ -17,6 +18,13 @@ def index(request):
     context = {
         'available_wool_list':wool_items
     }
+    save_product_path = "data/saved_products.json"
+    wool_json_list = []
+    for wool in wool_items:
+        w = model_to_dict(wool)
+        wool_json_list.append(w)
+    with open(save_product_path, 'w') as fp:
+        json.dump(wool_json_list, fp)
     return HttpResponse(template.render(context,request))
 
 
